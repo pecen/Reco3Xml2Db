1f1f89b68824b6a9863fb247d485aef09922f4ae
Peter Centellini <peter.centellini@scania.com>
2020-01-17 16:38:22 +0100

    Changed Release configuration in .csproj-file to use C# latest minor version

--- Reco3Xml2Db.UI.Module/Enums/PDSource.cs
+++ Reco3Xml2Db.UI.Module/Enums/PDSource.cs
@@ -2,7 +2,7 @@
 
 namespace Reco3Xml2Db.UI.Module.Enums {
   public enum PDSource { 
-    [Description("3dExperience")]
+    [Description("3DExperience")]
     Experience3D = 1,
     YDMC = 2,
     Unknown = 0
