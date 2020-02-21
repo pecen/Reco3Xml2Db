8e58a5e3c9b5c5e61654ea985bc404075b0a51f1
Peter Centellini <peter.centellini@scania.com>
2020-02-21 12:19:52 +0100

    Added initial part of Components grid

---
+++ Reco3Xml2Db.UI.Module/Views/ComponentsGrid.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Reco3Xml2Db.UI.Module.Views {
+  /// <summary>
+  /// Interaction logic for ComponentsGrid
+  /// </summary>
+  public partial class ComponentsGrid : UserControl {
+    public ComponentsGrid() {
+      InitializeComponent();
+    }
+  }
+}
