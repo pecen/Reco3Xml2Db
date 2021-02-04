11928b063bce7b947214f8b30750f9bf57286534
503212497 <peter.centellini@cytiva.com>
2021-02-04 17:27:05 +0100

    Added ModelBase. Change in .editorconfig.

---
+++ Reco3Xml2Db.UI.Module/Models/ModelBase.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Mvvm;
+
+namespace Reco3Xml2Db.UI.Module.Models {
+	public class ModelBase : BindableBase {
+		private bool _isChecked;
+		public bool IsChecked {
+			get => _isChecked;
+			set {
+				SetProperty(ref _isChecked, value);
+			}
+		}
+	}
+}
